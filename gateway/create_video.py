52b4597a1846950acb3d2e907410dea6f74e8bd9
sanvit <katrekarsanvit2@gmail.com>
2023-10-15 22:49:54 +0400

    Almost done

--- gateway/create_video.py
+++ gateway/create_video.py
@@ -1,7 +1,8 @@
 from moviepy.editor import *
 from urllib.request import urlretrieve 
-
+import shutil
 import ssl
+from final_llm import get_prediction
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -13,27 +14,35 @@ else:
 def create_video(video_details: list[dict]):
     start = 0
     clips = []
+    TEMP_FOLDER = 'tmp'
+    os.mkdir(TEMP_FOLDER)
     for i, video in enumerate(video_details):
-       name = f'video_{i}.mp4'
+       name = f'tmp/video_{i}.mp4'
        urlretrieve(video['link'], name)
-       clip = VideoFileClip(name).subclip(0, 5)
+       clip = VideoFileClip(name).subclip(0, 5).set_start(start)
        start += clip.duration
        clips.append(clip)
+    shutil.rmtree(TEMP_FOLDER)
     # concatenating both the clips
     final = CompositeVideoClip(clips)
     #writing the video into a file / saving the combined video
     final.write_videofile("merged.mp4")
 
-create_video(
-[{
-    "phrase": "Play",
-    "link": "https://player.vimeo.com/external/267630082.sd.mp4?s=cedc04d2f744731ef0c009efac27db351cd7e8c9&profile%20id=164&download=1"
-}, {
-    "phrase": "Sports union",
-    "link": "https://player.vimeo.com/external/267628422.sd.mp4?s=b05d2bb6228028d989fdde90d248292636f06d87&profile%20id=164&download=1"
-}, {
-    "phrase": "Painting",
-    "link": "https://player.vimeo.com/external/267523909.sd.mp4?s=939eda40b3188d6b4bcfbef2e5889d6e3a7a3e61&profile_id=164&download=1"
-}]  
-)
+if __name__ == '__main__':
+    '''
+    create_video(
+    [{
+        "phrase": "Play",
+        "link": "https://player.vimeo.com/external/267630082.sd.mp4?s=cedc04d2f744731ef0c009efac27db351cd7e8c9&profile%20id=164&download=1"
+    }, {
+        "phrase": "Sports union",
+        "link": "https://player.vimeo.com/external/267628422.sd.mp4?s=b05d2bb6228028d989fdde90d248292636f06d87&profile%20id=164&download=1"
+    }, {
+        "phrase": "Painting",
+        "link": "https://player.vimeo.com/external/267523909.sd.mp4?s=939eda40b3188d6b4bcfbef2e5889d6e3a7a3e61&profile_id=164&download=1"
+    }]  
+    )
+    '''
+    output = get_prediction("The Lion is swimming in the swimming pool")
+    create_video(output)
 
--- gateway/final_llm.py
+++ gateway/final_llm.py
@@ -1,113 +1,117 @@
 from langchain.llms.ollama import Ollama
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings import GPT4AllEmbeddings
+from langchain import hub
 import warnings
-
 
 warnings.filterwarnings('ignore')
 
-PROMPT = """
-[INST]
-<<SYS>>
-You are a sign language translator assistant.
-You will be given context of phrases mapped to a video link, of the format:
+def get_prediction(question: str) -> list[dict]:
+    PROMPT = """
+    [INST]
+    <<SYS>>
+    You are a sign language translator assistant.
+    You will be given context of phrases mapped to a video link, of the format:
 
-"First Phrase: First phrase video link" i.e., the context contains phrase and link delimited by a ':'
+    "First Phrase: First phrase video link" i.e., the context contains phrase and link delimited by a ':'
 
-For example, first phrase = Today
-Today: https://player.vimeo.com/external/516074516.sd.mp4?s=5a74961fe7e2d7374401234ccc5a3b1f407fc790&profile_id=164&download=1
+    For example, first phrase = Today
+    Today: https://player.vimeo.com/external/516074516.sd.mp4?s=5a74961fe7e2d7374401234ccc5a3b1f407fc790&profile_id=164&download=1
 
-Treat the above only as an example, do not use it in replies.
+    Treat the above only as an example, do not use it in replies.
 
-Consider the part starting with "https://" as "video link"
-So that, "Today video link" will mean "https://player.vimeo.com/external/516074516.sd.mp4?s=5a74961fe7e2d7374401234ccc5a3b1f407fc790&profile_id=164&download=1
+    Consider the part starting with "https://" as "video link"
+    So that, "Today video link" will mean "https://player.vimeo.com/external/516074516.sd.mp4?s=5a74961fe7e2d7374401234ccc5a3b1f407fc790&profile_id=164&download=1
 
-Your vocabulary is strictly restricted to the phrases provided in:
+    Your vocabulary is strictly restricted to the phrases provided in:
 
-{context}
+    {context}
 
-The above phrases are also presented in the document, used for question answering.
+    The above phrases are also presented in the document, used for question answering.
 
-Omit all grammar rules such that:
-1. Assistant translates for a language restricted to the vocabulary provided above.
-2. Assistant translates for a language without prepositions and grammatical phrases.
-3. Make meaningful translations.
-4. Replace phrases in the sentence with their synonyms or closest matches from the provided vocabulary.
-5. No prepositions or grammatical phrases in the translation, the sentence is simplified to just the main phrases.
-6. In case the phrase does not exist in the vocabulary, replace with individual letters.
-Just return the final output in the following format, assuming there are 'n' phrases:
-[{{
-    "phrase": "Closest matching phrase for phrase 1 provided in context accuratelt",
-    "link": "Exact link for phrase 1 provided in context accurately"
-}},
-{{
-    "phrase": "Closest matching phrase for phrase 2",
-    "link": "Exact link for phrase 2 provided in context"
-}},
-...,
-{{
-    "phrase": "Closest matching phrase for phrase n",
-    "link": "Exact link for the phrase n context"
-}}
-]
+    Omit all grammar rules such that:
+    1. Assistant translates for a language restricted to the vocabulary provided above.
+    2. Assistant translates for a language without prepositions and grammatical phrases.
+    3. Make meaningful translations.
+    4. Replace phrases in the sentence with their synonyms or closest matches from the provided vocabulary.
+    5. No prepositions or grammatical phrases in the translation, the sentence is simplified to just the main phrases.
+    6. In case the phrase does not exist in the vocabulary, replace with individual letters.
+    Just return the final output in the following format, assuming there are 'n' phrases:
+    [{{
+        "phrase": "Closest matching phrase for phrase 1 provided in context accuratelt",
+        "link": "Exact link for phrase 1 provided in context accurately"
+    }},
+    {{
+        "phrase": "Closest matching phrase for phrase 2",
+        "link": "Exact link for phrase 2 provided in context"
+    }},
+    ...,
+    {{
+        "phrase": "Closest matching phrase for phrase n",
+        "link": "Exact link for the phrase n context"
+    }}
+    ]
 
-wherein phrase 1, second phrase 2, phase 3, ..., phrase n, are all phrases present in the vocabulary provided to assistant.
+    wherein phrase 1, second phrase 2, phrase 3, ..., phrase n, are all phrases present in the vocabulary provided to assistant.
 
-Phrase should match exactly that of the phrase in the vocabulary provided as context.
+    Phrase strictly contains at most 2 words.
+    Phrase should match exactly that of the phrase in the vocabulary provided as context.
 
-Strictly use the format above and keep the answer concise.
+    Strictly use the format above and keep the answer concise.
 
-<<SYS>>
-Sentence to translate: {question}
-Answer: 
-[/INST]"""
+    <<SYS>>
+    Sentence to translate: {question}
+    Answer: 
+    [/INST]"""
 
 
-model = Ollama(
-    model="llama2",
-    num_gpu=10,
-    temperature=0.2,
-    top_k=10,
-    top_p=0.5
-)
+    model = Ollama(
+        model="llama2",
+        num_gpu=10,
+        temperature=0.2,
+        top_k=10,
+        top_p=0.5
+    )
 
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+    loader = TextLoader("data/en-data.txt")
+    data = loader.load()
 
-loader = TextLoader("en-data.txt")
-data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(data)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
 
-from langchain.vectorstores.chroma import Chroma
-from langchain.embeddings import GPT4AllEmbeddings
+    #question = "I love to swim in the swimming pool after painting"
+    #question = 'I love to paint before swimming'
+    #question = 'I love to play football at the sports union before painting'
+    #question = 'The knife is playing basketball'
+    docs = vectorstore.similarity_search(question)
 
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
+    rag_prompt_llama = hub.pull("rlm/rag-prompt-llama")
+    rag_prompt_llama.messages[0].prompt.template = PROMPT
 
-#question = "I love to swim in the swimming pool after painting"
-#question = 'I love to paint before swimming'
-#question = 'I love to play football at the sports union before painting'
-question = 'The knife is playing basketball'
-docs = vectorstore.similarity_search(question)
-#result = llm_chain(docs)
+    chain = load_qa_chain(model, chain_type="stuff", prompt=rag_prompt_llama)
 
-from langchain import hub
-from langchain.chains.question_answering import load_qa_chain
+    result = chain({"input_documents": docs, "question": question}, return_only_outputs=True)
 
-rag_prompt_llama = hub.pull("rlm/rag-prompt-llama")
-rag_prompt_llama.messages[0].prompt.template = PROMPT
+    print('\n\n')
 
-chain = load_qa_chain(model, chain_type="stuff", prompt=rag_prompt_llama)
+    output_str: str = result['output_text']
+    print(output_str)
+    try:
+        start_index = output_str.index('[')
+        end_index = output_str.index(']') + 1
+        output = eval(output_str[start_index: end_index])
+    except:
+        start_index = output_str.index('{')
+        end_index = output_str.index('}') + 1
+        output = [eval(output_str[start_index: end_index])]
 
-result = chain({"input_documents": docs, "question": question}, return_only_outputs=True)
+    return output
 
-print('\n\n')
-
-output_str: str = result['output_text']
-start_index = output_str.index('[')
-end_index = output_str.index(']') + 1
-
-print(output_str)
-
-output_dict = eval(output_str[start_index: end_index])
-
-print(output_dict)
+if __name__ == '__main__':
+    output = get_prediction("The Lion is swimming in the swimming pool")
+    print(output)
