e01205b40c26ee7a18d6bc58f4b8cd7f1b2bf037
bruhvya <bhavyakothari2910@gmail.com>
2023-10-19 21:28:12 +0400

    server functions

---
+++ src/server_functions/request.ts
@@ -0,0 +1,15 @@
+export async function makePost(sentence: string | null = null) {
+    const apiUrl = 'http://localhost:3000/predict'; 
+    const data = { english_sentence: sentence };
+  
+    const res = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  
+    return res;
+  }
+
